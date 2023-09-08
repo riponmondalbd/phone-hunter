c6cbf7965c767db4a9dd6ecac8a62643b9b5f334
Ripon Mondal <riponmondal149@gmail.com>
2023-09-08 16:39:24 +0600

    load data using phone hunter API

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const loadPhones = async () => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data[0]);
+}
+
+const displayPhones = phone => {
+    console.log(phone);
+}
+
+loadPhones();
