095f65d3867095a34c5cf71aea86133a85a0f833
Ripon Mondal <riponmondal149@gmail.com>
2023-09-08 17:21:45 +0600

    display phones and implement search functionality

--- js/app.js
+++ js/app.js
@@ -1,12 +1,39 @@
-const loadPhones = async () => {
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+const loadPhones = async (searchText) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data[0]);
+    displayPhones(data.data);
 }
 
-const displayPhones = phone => {
-    console.log(phone);
+const displayPhones = phones => {
+    const phonesContainer = document.getElementById('phone-container');
+
+    // clear the previous value before append new value
+    phonesContainer.textContent = '';
+
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+            <div class="card p-4">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural
+                        lead-in to additional
+                        content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        `;
+        phonesContainer.appendChild(phoneDiv);
+
+    })
 }
+
+document.getElementById('btn-search').addEventListener('click', function () {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText);
+})
 
 loadPhones();
