4c6e51b90435fae10b2880c5cdc2825d8c332a10
riponmondalbd <riponmondal149@gmail.com>
2024-06-19 22:07:24 +0600

    show consitional show all button

--- js/phone.js
+++ js/phone.js
@@ -16,6 +16,17 @@ const displayPhones = (phones) => {
   // clear phone container cards before adding new cards
   phoneContainer.textContent = "";
 
+  // display show all button if there are more than 12 phones
+  const showAllContainer = document.getElementById("show-all-container");
+  if (phones.length > 12) {
+    showAllContainer.classList.remove("hidden");
+  } else {
+    showAllContainer.classList.add("hidden");
+  }
+
+  // display only first 12 phones
+  phones = phones.slice(0, 12);
+
   phones.forEach((phone) => {
     // card div
     const phoneCard = document.createElement("div");
