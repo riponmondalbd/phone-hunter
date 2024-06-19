ee308f58b3e08fe9ca6f30e114fcee173d03f6b9
riponmondalbd <riponmondal149@gmail.com>
2024-06-19 22:44:22 +0600

    implement show all button to display all data

--- js/phone.js
+++ js/phone.js
@@ -1,14 +1,14 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
   const phones = data.data;
   // console.log(phones)
-  displayPhones(phones);
+  displayPhones(phones, isShowAll);
 };
 
-const displayPhones = (phones) => {
+const displayPhones = (phones, isShowAll) => {
   // console.log(phones);
 
   // get card container
@@ -18,14 +18,16 @@ const displayPhones = (phones) => {
 
   // display show all button if there are more than 12 phones
   const showAllContainer = document.getElementById("show-all-container");
-  if (phones.length > 12) {
+  if (phones.length > 12 && !isShowAll) {
     showAllContainer.classList.remove("hidden");
   } else {
     showAllContainer.classList.add("hidden");
   }
 
-  // display only first 12 phones
-  phones = phones.slice(0, 12);
+  // display only first 12 phones if not show all
+  if (!isShowAll) {
+    phones = phones.slice(0, 12);
+  }
 
   phones.forEach((phone) => {
     // card div
@@ -52,13 +54,13 @@ const displayPhones = (phones) => {
 };
 
 // handle search button
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
   // start spinner
   toggleLoadingSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
-  console.log(searchText);
-  loadPhone(searchText);
+  //   console.log(searchText);
+  loadPhone(searchText, isShowAll);
 };
 
 const toggleLoadingSpinner = (isLoading) => {
@@ -68,6 +70,11 @@ const toggleLoadingSpinner = (isLoading) => {
   } else {
     loadingSpinner.classList.add("hidden");
   }
+};
+
+// handle show all
+const handleShowAll = () => {
+  handleSearch(true);
 };
 
 // loadPhone("apple");
